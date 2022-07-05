3aa0854c06ec99416b494d1aecace3fb7cdd03c4
Simon Baumhauer <simon.baumhauer@hotmail.ch>
2022-07-05 10:06:20 +0200

    router link for component details changed

--- src/app/country-details/country-details.component.ts
+++ src/app/country-details/country-details.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-country-details',
   templateUrl: './country-details.component.html',
-  styleUrls: ['./country-details.component.scss']
+  styleUrls: ['./country-details.component.scss'],
 })
 export class CountryDetailsComponent implements OnInit {
+  @Input() currentCountry: string = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
